0f16fb23a2dcf95f478cb3ce30988692672d782e
frio anbia <frioanbia63@gmail.com>
2023-09-25 12:38:46 +0700

    Initial Commit

---
+++ Quiz-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz-iOS
+//
+//  Created by Frio Anbia on 25/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
